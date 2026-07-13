b3dac291118e5ec164896e57ea1e7c9622e2455e
Alex F. <acfabrizio@gmail.com>
2026-07-13 10:17:13 -0400

    Attempts at week one problem set

---
+++ Python/Lesson 01 - Python Basics/weekly_problem_sets_1.py
@@ -0,0 +1,11 @@
+# Attempt at the weekly problem sets
+
+# introduction card attempt
+name = input("What is your first name? ")
+fav_num = int(input("What is your Favorite number? "))
+
+print("*" * fav_num)
+print("*   ", "Hello,", name + "!")
+
+
+print("*" * fav_num)
