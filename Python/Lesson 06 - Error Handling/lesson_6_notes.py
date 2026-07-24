5158d192da999bce2118bf53a48a4c4e0f74b39d
Alex F. <acfabrizio@gmail.com>
2026-07-24 12:07:54 -0400

    lesson 6 notes

---
+++ Python/Lesson 06 - Error Handling/lesson_6_notes.py
@@ -0,0 +1,29 @@
+# import random
+
+# with open("file_input.txt", "w") as f:
+#     for line in range(1, 100):
+#         rand_num = random.randint(50, 100)
+#         f.write(str(rand_num) + "\n")
+
+# with open("file_input.txt", "r") as input_f:
+#     lines = input_f.readlines()
+#     count = 0
+#     min = 1000
+#     max = 0
+#     sum = 0
+#     for line in lines:
+#         amount = int(line)
+#         sum += amount
+#         count += 1
+#         if amount > max:
+#             max = amount
+#         if amount < min:
+#             min = amount
+#     average = sum / count
+
+#     print(f"Max is : {max} Min is: {min} Average is {average}")
+
+
+classful = [1, 2, 3, 4, 5]
+
+print(classful[6])
